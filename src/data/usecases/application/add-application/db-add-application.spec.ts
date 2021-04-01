16693facac5664439860dfb373c24fbe1d84d836
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-01 15:00:55 -0300

    feat: ensure DbAddApplication calls Hasher with correct values

--- src/data/usecases/application/add-application/db-add-application.spec.ts
+++ src/data/usecases/application/add-application/db-add-application.spec.ts
@@ -1,18 +1,23 @@
+import { Hasher } from '@/data/protocols/criptography/hasher'
 import { AddApplicationRepository } from '@/data/protocols/db/application/add-application-repository'
+import { mockHasher } from '@/data/test/mock-criptography'
 import { mockAddApplicationRepository } from '@/data/test/mock-db-application'
-import { mockApplicationParams } from '@/domain/test/mock-application'
+import { mockApplicationModel, mockApplicationParams } from '@/domain/test/mock-application'
 import { DbAddApplication } from './db-add-application'
 
 type SutTypes = {
   sut: DbAddApplication
   addApplicationRepositoryStub: AddApplicationRepository
+  hasherStub: Hasher
 }
 
 const mockSut = (): SutTypes => {
+  const hasherStub = mockHasher()
   const addApplicationRepositoryStub = mockAddApplicationRepository()
   return {
-    sut: new DbAddApplication(addApplicationRepositoryStub),
-    addApplicationRepositoryStub
+    sut: new DbAddApplication(addApplicationRepositoryStub, hasherStub),
+    addApplicationRepositoryStub,
+    hasherStub
   }
 }
 
@@ -28,5 +33,11 @@ describe('AddApplication usecase', () => {
     jest.spyOn(addApplicationRepositoryStub, 'add').mockRejectedValueOnce(new Error())
     const promise = sut.add(mockApplicationParams())
     await expect(promise).rejects.toThrow()
+  })
+  test('Should call Hasher with correct value', async () => {
+    const { sut, hasherStub } = mockSut()
+    const encryptSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.add(mockApplicationParams())
+    expect(encryptSpy).toHaveBeenCalledWith({ application: mockApplicationModel().id })
   })
 })
--- src/data/usecases/application/add-application/db-add-application.ts
+++ src/data/usecases/application/add-application/db-add-application.ts
@@ -1,13 +1,17 @@
+import { Hasher } from '@/data/protocols/criptography/hasher'
 import { AddApplicationRepository } from '@/data/protocols/db/application/add-application-repository'
 import { ApplicationModel } from '@/domain/models/application'
 import { AddApplication, AddApplicationParams } from '@/domain/usecases/application/add-application'
 
 export class DbAddApplication implements AddApplication {
   constructor (
-    private readonly addApplicationRepository: AddApplicationRepository
+    private readonly addApplicationRepository: AddApplicationRepository,
+    private readonly hasher: Hasher
   ) {}
 
-  async add (application: AddApplicationParams): Promise<ApplicationModel> {
-    return await this.addApplicationRepository.add(application)
+  async add (applicationParams: AddApplicationParams): Promise<ApplicationModel> {
+    const application = await this.addApplicationRepository.add(applicationParams)
+    await this.hasher.hash({ application: application.id })
+    return application
   }
 }
